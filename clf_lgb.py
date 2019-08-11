aab55af247a61fe406b98bea76356e77ec818a1b
kryptonite0 <giulia.savorgnan@gmail.com>
2019-08-11 19:33:27 +0000

    clf script

---
+++ clf_lgb.py
@@ -0,0 +1,103 @@
+import pandas as pd
+import os
+import time
+import lightgbm as lgb
+import matplotlib.pyplot as plt
+import warnings
+from datetime import datetime
+from bayes_opt import BayesianOptimization
+from lightgbm import LGBMClassifier
+import numpy as np
+
+import settings_model
+
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+class color:
+    PURPLE = '\033[95m'
+    CYAN = '\033[96m'
+    DARKCYAN = '\033[36m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    END = '\033[0m'
+    
+
+# def accuracy(preds, train_data):
+#     labels = train_data.get_label()
+#     pred = np.argmax(preds.reshape(n_classes, len(preds)//n_classes), axis=0)
+#     return 'accuracy', np.mean(labels == pred), True
+
+def lgbm_evaluate(**params):
+    start = time.time()
+    params['num_leaves'] = int(params['num_leaves'])
+    params['max_depth'] = int(params['max_depth'])
+        
+    clf = LGBMClassifier(**params, 
+                         n_estimators=10000, 
+                         n_jobs=os.cpu_count(),
+                         objective="multiclass",
+                         num_class=n_classes,
+                        )
+        
+    clf.fit(df_train[features].values, df_train["TARGET"].values, 
+            eval_set = [(df_train[features].values, df_train["TARGET"].values),
+                        (df_valid[features].values, df_valid["TARGET"].values)],
+            early_stopping_rounds=10, verbose=0)
+    
+#     train_preds = clf.predict_proba(df_train[features].values, num_iteration=clf.best_iteration_)
+    valid_preds = clf.predict_proba(df_valid[features].values, num_iteration=clf.best_iteration_)
+    
+#     print('Accuracy train {:.6f}'.format(sum(np.argmax(train_preds, axis=1) == df_train['TARGET'].values) / float(len(train_preds))))
+    acc_valid = np.mean(np.argmax(valid_preds, axis=1) == df_valid['TARGET'].values)
+        
+    return acc_valid
+
+def optimize_lgbm():
+    
+    params_space = {'colsample_bytree': (0.9, 1.0),
+                    'learning_rate': (0.01, 1.0), 
+                    'num_leaves': (20, 1000), 
+                    'subsample': (0.5, 1.0), 
+                    'max_depth': (2, 1000), 
+                    'reg_alpha': (0.0, 1.0), 
+                    'reg_lambda': (0.0, 1.0), 
+                    'min_split_gain': (0.0001, 1.),
+                    'min_child_weight': (5., 200.),
+                   }
+
+    bo = BayesianOptimization(lgbm_evaluate, params_space)
+    bo.maximize(init_points=50, n_iter=50)
+    
+    best_acc = bo.max['target']
+    best_params = bo.max['params']
+    best_params['num_leaves'] = int(best_params['num_leaves'])
+    best_params['max_depth'] = int(best_params['max_depth'])
+    
+    
+    print("Best validation acc: {}".format(best_acc))
+    print('Best parameters found by optimization:\n')
+    for k, v in best_params.items():
+        print(color.BLUE + k + color.END + ' = ' + color.BOLD + str(v)+ color.END + '     [',params_space[k],']')
+        
+    return best_acc, best_params
+
+# model_id = "HUVEC_20190810_202545"
+model_id = "HEPG2_20190811_142600"
+print(model_id)
+output_dir = os.path.join(settings_model.root_path, "models", "siamese-cell",
+                          f"{model_id}", "emb")
+
+df_train = pd.read_csv(os.path.join(output_dir, "emb_train.csv"), header=None)
+df_valid = pd.read_csv(os.path.join(output_dir, "emb_valid.csv"), header=None)
+df_train.columns = df_train.columns.tolist()[:-1] + ["TARGET"]
+df_valid.columns = df_valid.columns.tolist()[:-1] + ["TARGET"]
+
+features = df_train.columns.tolist()[:-1]
+n_classes = df_train["TARGET"].nunique()
+labels_valid = df_valid["TARGET"].unique()
+
+best_acc, best_params = optimize_lgbm()
